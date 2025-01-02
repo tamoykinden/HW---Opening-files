989847e791be9c34e819276f861406f59d61ddc5
Denis Tamoykin <denistamoykin04@gmail.com>
2025-01-02 11:46:31 +0300

    Add next func

--- Task 1.py
+++ Task 1.py
@@ -8,3 +8,22 @@ def read_file(file_path):
         for line in f:
             list_of_lines.append(line.strip())
     return list_of_lines
+rec = read_file("recipes.txt")
+print (rec)
+def chg_in_dict (lines):
+    """
+    Парсинг рецепта из списка строк.
+    """
+    recipe_name = lines[0].strip()
+    ing_count = int(lines[1].strip())
+    ingr = []
+    for line in lines[2:2 + ing_count]:
+        ingredient_name, quantity, measure = line.strip().split(' | ')
+        ingr.append({
+            'ingredient_name': ingredient_name,
+            'quantity': int(quantity),
+            'measure': measure
+        })
+    return recipe_name, ingr 
+res = chg_in_dict(rec)
+print(res)
